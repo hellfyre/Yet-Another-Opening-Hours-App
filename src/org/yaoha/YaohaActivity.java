397cb6801858931674a11dc6e689a68d378e57b4
Matthias Uschok <dev@uschok.de>
2011-11-18 16:14:42 +0100

    Added test button to call map view

--- src/org/yaoha/YaohaActivity.java
+++ src/org/yaoha/YaohaActivity.java
@@ -3,17 +3,25 @@ package org.yaoha;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class YaohaActivity extends Activity {
+public class YaohaActivity extends Activity implements OnClickListener {
+	Button mapButton;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mapButton = (Button) findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(this);
     }
     
     @Override
@@ -25,19 +33,24 @@ public class YaohaActivity extends Activity {
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
-		case R.id.osm_account:
-			Intent i = new Intent(this, SettingsActivity.class);
-			startActivity(i);
-			return true;
-		case R.id.quit_app:
-			this.finish();
-			return true;
-		case R.id.buy_pro:
-			Toast.makeText(this, "You just payed 49,99€. Enjoy this Pro-Version!", Toast.LENGTH_LONG).show();
-			return true;
-		default:
-			return false;
-		}
+        switch (item.getItemId()) {
+            case R.id.osm_account:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.quit_app:
+                this.finish();
+                return true;
+            case R.id.buy_pro:
+                Toast.makeText(this, "You just payed 49,99€. Enjoy this Pro-Version!", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return false;
+        }
     }
+
+	@Override
+	public void onClick(View v) {
+		  startActivity(new Intent(this, YaohaMapActivity.class));
+	}
 }
