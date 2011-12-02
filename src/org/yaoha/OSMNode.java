2c5769a98715c5e621660ca40ab32f204b53e812
Matthias Uschok <dev@uschok.de>
2011-12-02 19:15:26 +0100

    Added important member vars
    We'll always be interested in the amenity and opening_hours values, so
    they should be easily accessible

--- src/org/yaoha/OSMNode.java
+++ src/org/yaoha/OSMNode.java
@@ -1,23 +1,28 @@
 package org.yaoha;
 
+import java.util.Date;
 import java.util.HashMap;
 
 public class OSMNode {
     private int ID;
     private int latitudeE6;
     private int longitudeE6;
+    private String name;
+    private String amenity;
+    private String opening_hours;
+    private Date lastUpdated;
     private HashMap<String, String> attributes;
-    
-    public OSMNode(int ID, int latitudeE6, int longitudeE6) {
-        this.ID = ID;
-        this.latitudeE6 = latitudeE6;
-        this.longitudeE6 = longitudeE6;
-    }
     
     public OSMNode(String ID, String latitude, String longitude) {
         this.ID = Integer.parseInt(ID);
         this.latitudeE6 = new Double(Double.parseDouble(latitude)).intValue()*1000000;
         this.longitudeE6 = new Double(Double.parseDouble(longitude)).intValue()*1000000;
+    }
+    
+    public OSMNode(int ID, int latitudeE6, int longitudeE6) {
+        this.ID = ID;
+        this.latitudeE6 = latitudeE6;
+        this.longitudeE6 = longitudeE6;
     }
     
     public void putAttribute(String key, String value) {
@@ -38,6 +43,38 @@ public class OSMNode {
     
     public int getLongitudeE6() {
         return longitudeE6;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAmenity() {
+        return amenity;
+    }
+
+    public void setAmenity(String amenity) {
+        this.amenity = amenity;
+    }
+
+    public String getOpening_hours() {
+        return opening_hours;
+    }
+
+    public void setOpening_hours(String opening_hours) {
+        this.opening_hours = opening_hours;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
     }
     
 }
