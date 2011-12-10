364b240e9f364dec111b3e12b15a94be0ecef22c
Matthias Uschok <dev@uschok.de>
2011-12-10 22:44:27 +0100

    Replaced IntentService by AsyncTask
    This makes callback handling a hell of a lot easier

---
+++ src/org/yaoha/OsmNodeRetrieverTask.java
@@ -0,0 +1,39 @@
+package org.yaoha;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import android.os.AsyncTask;
+
+public class OsmNodeRetrieverTask extends AsyncTask<String, Void, Void> {
+    private ArrayList<OsmNodeRetrieverListener> receiverList = new ArrayList<OsmNodeRetrieverListener>();
+
+    @Override
+    protected Void doInBackground(String... params) {
+        String requestString = "";
+        if (params.length == 1) {
+            requestString = params[0];
+        }
+        
+        OverpassConnector connector = new OverpassConnector();
+        InputStream in = connector.getResponseInputStream(requestString);
+        
+        OsmXmlParser parser = new OsmXmlParser();
+        parser.parse(in, Nodes.getInstance().getNodeMap());
+        
+        callListeners();
+        
+        return null;
+    }
+    
+    public void addListener(OsmNodeRetrieverListener receiver) {
+        receiverList.add(receiver);
+    }
+    
+    private void callListeners() {
+        for (OsmNodeRetrieverListener receiver : receiverList) {
+            receiver.onRequestComplete();
+        }
+    }
+
+}
