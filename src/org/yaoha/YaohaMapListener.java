a7f58b9421b504c1bf087caf94fc8e980aef30f8
Lutz <gnudgf@gmx.net>
2011-12-11 00:35:07 +0100

    when moving the map, only retrieve shop data of new shops

--- src/org/yaoha/YaohaMapListener.java
+++ src/org/yaoha/YaohaMapListener.java
@@ -32,8 +32,6 @@ public class YaohaMapListener implements MapListener, OsmNodeRetrieverListener {
             if (updateAmenities) {
                 update(arg0.getSource().getBoundingBox());
             }
-            boundingBox = arg0.getSource().getBoundingBox();
-
             return true;
         }
         return false;
@@ -51,57 +49,90 @@ public class YaohaMapListener implements MapListener, OsmNodeRetrieverListener {
                 updateAmenities = false;
             }
             zoomLevel = newZoomLevel;
-            boundingBox = event.getSource().getBoundingBox();
-
             return true;
         }
         return false;
     }
     
     private void update(BoundingBoxE6 bbox) {
-        String latLow = "";
-        String latHigh = "";
-        String lonLow = "";
-        String lonHigh = "";
-        if ( (bbox.getLatNorthE6() == bbox.getLatSouthE6()) || (bbox.getLonEastE6() == bbox.getLonWestE6()) ) return;
+        if ( (bbox.getLatNorthE6() == bbox.getLatSouthE6()) || (bbox.getLonEastE6() == bbox.getLonWestE6()) )
+            return;
         
-        if (bbox.getLatNorthE6() > bbox.getLatSouthE6()) {
-            latHigh = Double.toString(bbox.getLatNorthE6()/1000000.0);
-            latLow = Double.toString(bbox.getLatSouthE6()/1000000.0);
-        }
-        else {
-            latLow =Double.toString( bbox.getLatNorthE6()/1000000.0);
-            latHigh = Double.toString(bbox.getLatSouthE6()/1000000.0);
-        }
-        if (bbox.getLonEastE6() > bbox.getLonWestE6()) {
-            lonHigh = Double.toString(bbox.getLonEastE6()/1000000.0);
-            lonLow = Double.toString(bbox.getLonWestE6()/1000000.0);
-        }
-        else {
-            lonLow = Double.toString(bbox.getLonEastE6()/1000000.0);
-            lonHigh = Double.toString(bbox.getLonWestE6()/1000000.0);
+        // check for intersection between old and new bounding box
+        if (this.boundingBox != null 
+                && (this.boundingBox.getLatNorthE6() < bbox.getLatSouthE6() 
+                        || this.boundingBox.getLatSouthE6() > bbox.getLatNorthE6()
+                        || this.boundingBox.getLonEastE6() < bbox.getLonWestE6()
+                        || this.boundingBox.getLonWestE6() > bbox.getLonEastE6())
+                        ) {
+            // no intersection found
+            this.boundingBox = null;
         }
         
-        latLow = latLow.replace(',', '.');
-        latHigh = latHigh.replace(',', '.');
-        lonLow = lonLow.replace(',', '.');
-        lonHigh = lonHigh.replace(',', '.');
+        if (this.boundingBox == null) {
+            queryShopsInRectangle(bbox.getLatNorthE6(), bbox.getLatSouthE6(), bbox.getLonEastE6(), bbox.getLonWestE6());
+        } else {
+            // bounding box moved
+            // assumes zooming out, too
+            int northdiff = this.boundingBox.getLatNorthE6() - bbox.getLatNorthE6();
+            int southdiff = this.boundingBox.getLatSouthE6() - bbox.getLatSouthE6();
+            int eastdiff = this.boundingBox.getLonEastE6() - bbox.getLonEastE6();
+            int westdiff = this.boundingBox.getLonWestE6() - bbox.getLonWestE6();
+            
+            // moved north
+            if (northdiff < 0) {
+                queryShopsInRectangle(bbox.getLatNorthE6(), this.boundingBox.getLatNorthE6(), this.boundingBox.getLonWestE6(), this.boundingBox.getLonEastE6());
+            }
+            // moved south
+            if (southdiff > 0) {
+                queryShopsInRectangle(this.boundingBox.getLatSouthE6(), bbox.getLatSouthE6(), this.boundingBox.getLonWestE6(), this.boundingBox.getLonEastE6());
+            }
+            // with latitude of bbox to get the entire height
+            // moved east
+            if (eastdiff < 0) {
+                queryShopsInRectangle(bbox.getLatNorthE6(), bbox.getLatSouthE6(), bbox.getLonEastE6(), this.boundingBox.getLonEastE6());
+            }
+            // moved west
+            if (westdiff > 0) {
+                queryShopsInRectangle(bbox.getLatNorthE6(), bbox.getLatSouthE6(), this.boundingBox.getLonWestE6(), bbox.getLonWestE6());
+            }
+
+        }
+        
+        this.boundingBox = bbox;
+    }
+    
+    void queryShopsInRectangle(double latHigh, double latLow, double lonHigh, double lonLow) {
+        if (latHigh < latLow) {
+            double tmp = latLow;
+            latLow = latHigh;
+            latHigh = tmp;
+        }
+        
+        if (lonHigh < lonLow) {
+            double tmp = lonLow;
+            lonLow = lonHigh;
+            lonHigh = tmp;
+        }
+        
+        String latLows = Double.toString(latLow/1e6);
+        String latHighs = Double.toString(latHigh/1e6);
+        String lonLows = Double.toString(lonLow/1e6);
+        String lonHighs = Double.toString(lonHigh/1e6);
+        
+        latLows = latLows.replace(',', '.');
+        latHighs = latHighs.replace(',', '.');
+        lonLows = lonLows.replace(',', '.');
+        lonHighs = lonHighs.replace(',', '.');
         
         OsmNodeRetrieverTask task = new OsmNodeRetrieverTask();
         task.addListener(this);
-        String requestString = "node[bbox=" + lonLow + "," + latLow + "," + lonHigh + "," + latHigh + "][opening_hours=*]";
+        String requestString = "node[bbox=" + lonLows + "," + latLows + "," + lonHighs + "," + latHighs + "][opening_hours=*]";
         task.execute(requestString);
         
         Log.d("YaohaMapListener", "Updated; request String: " + requestString);
-        /*
-        if (this.boundingBox == null) {
-            this.mapActivity.updateShops(bbox.getLatNorthE6(), bbox.getLatSouthE6(), bbox.getLonEastE6(), bbox.getLonWestE6());
-        } else {
-            // TODO the user moved the map, only retrieve the shops for the new map content
-        }
-        */
     }
-
+    
     @Override
     public void onRequestComplete() {
         Log.d("YaohaMapListener", "callback called");
