8ab305746a32e454e4786f111f8e3335e2b796c5
Hobohm <s.hobohm@tu-bs.de>
2011-12-13 14:39:55 +0100

    add possibility to add favorite stores

--- src/org/yaoha/YaohaActivity.java
+++ src/org/yaoha/YaohaActivity.java
@@ -1,24 +1,30 @@
 package org.yaoha;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.Toast;
 
 public class YaohaActivity extends Activity implements OnClickListener {
 	Button mapButton;
 	Button startButton;
+	Button button_favorite_1, button_favorite_2, button_favorite_3;
 	private static final String[] SHOP_TYPES = new String[] {
         "groceries", "computer", "sport", "clothes", "gas station"
     };
@@ -29,21 +35,27 @@ public class YaohaActivity extends Activity implements OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(getBaseContext()); 
-        if (prefs.getBoolean("start_with_map",false) == true) {
-            MultiAutoCompleteTextView textView = (MultiAutoCompleteTextView) findViewById(R.id.searchTextfield);
-            
-            Intent intent = new Intent(this, YaohaMapActivity.class);
-            intent.putExtra("org.yaoha.YaohaMapActivity.SearchText", textView.getText());
-            startActivity(intent);
-		}
+//        SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(getBaseContext()); 
+//        if (prefs.getBoolean("start_with_map",false) == true) {
+//            //startButton.performClick();
+//            //TODO make this work
+//		}
         startButton = (Button) findViewById(R.id.button_start);
         startButton.setOnClickListener(this);
+        button_favorite_1 = (Button) findViewById(R.id.button_fav_1);
+        button_favorite_1.setOnClickListener(this);
+        button_favorite_2 = (Button) findViewById(R.id.button_fav_2);
+        button_favorite_2.setOnClickListener(this);
+        //button_favorite_3 = (Button) findViewById(R.id.button_fav_3);
+        //button_favorite_3.setOnClickListener(this);
         
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, SHOP_TYPES);
         MultiAutoCompleteTextView textView = (MultiAutoCompleteTextView) findViewById(R.id.searchTextfield);
         textView.setAdapter(adapter);
         textView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        
+        registerForContextMenu(button_favorite_1);
+        registerForContextMenu(button_favorite_2);
     }
     
     @Override
@@ -75,10 +87,69 @@ public class YaohaActivity extends Activity implements OnClickListener {
     public void onClick(View v) {
         if(v.getId() == R.id.button_start) {
             MultiAutoCompleteTextView textView = (MultiAutoCompleteTextView) findViewById(R.id.searchTextfield);
-            
-            Intent intent = new Intent(this, YaohaMapActivity.class);
-            intent.putExtra("org.yaoha.YaohaMapActivity.SearchText", textView.getText());
-            startActivity(intent);
+            searchMapWithKey(textView.getText());
         }
+        if(v.getId() == R.id.button_fav_1) {
+            if (button_favorite_1.equals("+")) {
+                Toast.makeText(this, "Hold button to edit.", Toast.LENGTH_SHORT).show();
+            } else {
+                searchMapWithKey(button_favorite_1.getText());
+            }
+        }
+    }
+    
+    public void searchMapWithKey(CharSequence keyword){
+        Intent intent = new Intent(this, YaohaMapActivity.class);
+        intent.putExtra("org.yaoha.YaohaMapActivity.SearchText", keyword);
+        startActivity(intent); 
+    }
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+            ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Context Menu");  
+        menu.add(0, v.getId(), 0, "Add/edit this favorite");  
+        menu.add(0, v.getId(), 0, "Remove this favorite");
+        
+    }
+    
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == button_favorite_1.getId()){
+            editFavs(item, button_favorite_1);
+        }else if (item.getItemId() == button_favorite_2.getId()){
+            editFavs(item, button_favorite_2);
+        }
+    return super.onContextItemSelected(item);
+    }
+    
+    public boolean editFavs(MenuItem item, final Button btn){
+        if(item.getTitle()=="Add/edit this favorite"){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this); 
+            final EditText  input = new EditText(this);
+            alert.setTitle("Adding favorite"); 
+            alert.setMessage("Enter your favorite search"); 
+            alert.setView(input); 
+
+            alert.setPositiveButton("Set", new DialogInterface.OnClickListener() { 
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    btn.setText(input.getText());
+                } 
+                }); 
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() { 
+                  public void onClick(DialogInterface dialog, int whichButton) { 
+                    // Canceled. 
+                  } 
+                }); 
+            alert.show();
+        } else if (item.getTitle()=="Remove this favorite"){
+           btn.setText("+");
+        } else {
+            Toast.makeText(this, "Placeholder - You schould never see this.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return super.onContextItemSelected(item);
     }
 }
