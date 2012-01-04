d93d7d3c6053c40360fc8916dc7d5e4f877cf234
Lutz <lutz.reinhardt@tu-bs.de>
2012-01-04 14:37:55 +0100

    untestet version of our DbHelper

--- src/org/yaoha/OsmNodeDbHelper.java
+++ src/org/yaoha/OsmNodeDbHelper.java
@@ -3,6 +3,7 @@ package org.yaoha;
 import java.util.HashMap;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteStatement;
@@ -18,11 +19,6 @@ public class OsmNodeDbHelper extends SQLiteOpenHelper {
     private static final String nodesAttributesTableName = "nodes_attr";
     private static final String nodesAttributesTableKey = "key";
     private static final String nodesAttributesTableValue = "value";
-    SQLiteDatabase db;
-    SQLiteStatement queryNode;
-    SQLiteStatement queryNodes;
-    SQLiteStatement queryAttributes;
-    SQLiteStatement queryNodesFromMapExtract;
     SQLiteStatement insertNode;
     SQLiteStatement insertAttribute;
 
@@ -32,7 +28,6 @@ public class OsmNodeDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        this.db = db;
         db.execSQL("CREATE TABLE " + nodesTableName + " (" + nodesTablePrimaryKey + " INTEGER PRIMARY KEY NOT NULL, "
                 + nodesTableLatitude + " INTEGER NOT NULL, "
                 + nodesTableLongitude + " INTEGER NOT NULL; ");
@@ -41,34 +36,89 @@ public class OsmNodeDbHelper extends SQLiteOpenHelper {
                 + nodesAttributesTableValue + " TEXT, "
                 + "FOREIGN KEY (" + nodesTablePrimaryKey + ") REFERENCES nodes (" + nodesTablePrimaryKey + "), "
                 + "PRIMARY KEY (" + nodesTablePrimaryKey + "," + nodesAttributesTableKey + "," + nodesAttributesTableValue + "));");
-        queryNode = db.compileStatement("SELECT * FROM " + nodesTableName + " WHERE " + nodesTablePrimaryKey + " = ?;");
-        queryNodes = db.compileStatement("SELECT * FROM " + nodesTableName + ";");
-        queryAttributes = db.compileStatement("SELECT * FROM " + nodesAttributesTableName + " WHERE " + nodesTablePrimaryKey + " = ?;");
-        queryNodesFromMapExtract = null; // TODO
         insertNode = db.compileStatement("INSERT INTO " + nodesTableName + " (" + nodesTablePrimaryKey + "," + nodesTableLatitude + "," + nodesTableLongitude + ") VALUES (?,?,?);");
         insertAttribute = db.compileStatement("INSERT INTO " + nodesAttributesTableName + " (" + nodesTablePrimaryKey + "," + nodesAttributesTableKey + "," + nodesAttributesTableValue + ") VALUES (?,?,?);");
+    }
+    
+    private Cursor queryNodes() {
+    	// "SELECT * FROM " + nodesTableName + ";"
+    	SQLiteDatabase db = getReadableDatabase();
+    	return db.query(nodesTableName, null, null, null, null, null, null);
+    }
+    
+    private Cursor queryAttributes(int key) {
+    	// "SELECT * FROM " + nodesAttributesTableName + " WHERE " + nodesTablePrimaryKey + " = ?;"
+    	SQLiteDatabase db = getReadableDatabase();
+    	return db.query(nodesAttributesTableName, null, nodesTablePrimaryKey + " = ?", new String[] {"" + key}, null, null, null);
+    }
+
+    // TODO we should optimize the database for rangequeries containing latitude and longitude
+    private Cursor queryNodesFromMapExtract(int left, int top, int right, int bottom) {
+    	SQLiteDatabase db = getReadableDatabase();
+    	return db.query(nodesTableName, null,
+    			nodesTableLongitude + " >= " + left + "AND " + nodesTableLongitude + " <= " + right + " AND " + nodesTableLatitude + " >= " + bottom + " AND " + nodesTableLatitude + " <= " + top,
+    			new String[] {"" + left, "" + right, "" + bottom, "" + top}, null, null, null);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.w("OsmNodeProvider", "Upgrading database from version "
                 + oldVersion + " to " + newVersion);
-        db.execSQL("DROP TABLE IF EXISTS nodes;");
-        db.execSQL("DROP TABLE IF EXISTS nodes_attr;");
+        db.execSQL("DROP TABLE IF EXISTS " + nodesTableName + ";");
+        db.execSQL("DROP TABLE IF EXISTS " + nodesAttributesTableName + ";");
         onCreate(db);
     }
     
     public void storeNode(OsmNode node) {
-        
+        insertNode.bindLong(0, node.getID());
+        insertNode.bindLong(1, node.getLongitudeE6());
+        insertNode.bindLong(2, node.getLatitudeE6());
+        insertNode.executeInsert();
+        insertNode.clearBindings();
+        for (String key : node.getKeys()) {
+            String value = node.getAttribute(key);
+            insertAttribute.bindLong(0, node.getID());
+            insertAttribute.bindString(1, key);
+            insertAttribute.bindString(2, value);
+            insertAttribute.executeInsert();
+            insertAttribute.clearBindings();
+        }
     }
     
-    public OsmNode getNode(int key) {
-        
-        return null;
+    private OsmNode createNodeFromRow(Cursor c) {
+    	int keyIndex = c.getColumnIndexOrThrow(nodesTablePrimaryKey);
+    	int latIndex = c.getColumnIndexOrThrow(nodesTableLatitude);
+        int lonIndex = c.getColumnIndexOrThrow(nodesTableLongitude);
+        return new OsmNode(c.getInt(keyIndex), c.getInt(latIndex), c.getInt(lonIndex));
     }
     
-    public HashMap<Integer, OsmNode> createNodesFromDatabase() {
-        return null;
+    private void addAttributesToNode(OsmNode node) {
+    	Cursor c = queryAttributes(node.getID());
+        for (int i = 0; i < c.getCount(); i++, c.moveToNext()) {
+        	int keyIndex = c.getColumnIndexOrThrow(nodesAttributesTableKey);
+        	int valueIndex = c.getColumnIndexOrThrow(nodesAttributesTableValue);
+        	node.putAttribute(c.getString(keyIndex), c.getString(valueIndex));
+        }
+        c.close();
+    }
+    
+    public HashMap<Integer, OsmNode> createNodesFromEntireDatabase() {
+    	return createNodesFromRows(queryNodes());
+    }
+    
+    public HashMap<Integer, OsmNode> createNodesFromMapExtract(int left, int top, int right, int bottom) {
+    	return createNodesFromRows(queryNodesFromMapExtract(left, top, right, bottom));
+    }
+    
+    private HashMap<Integer, OsmNode> createNodesFromRows(Cursor c) {
+    	HashMap<Integer, OsmNode> nodesMap = new HashMap<Integer, OsmNode>();
+    	for (int i = 0; i < c.getCount(); i++, c.moveToNext()) {
+    		OsmNode node = createNodeFromRow(c);
+    		addAttributesToNode(node);
+    		nodesMap.put(node.getID(), node);
+    	}
+    	c.close();
+        return nodesMap;
     }
 
 }
