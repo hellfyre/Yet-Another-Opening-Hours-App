57e6dd0bd5d7272454ea353d6bd70e94e7ae8a60
Matthias Uschok <dev@uschok.de>
2012-02-04 15:44:16 +0100

    Added Basic Auth

--- src/org/yaoha/ApiConnector.java
+++ src/org/yaoha/ApiConnector.java
@@ -2,7 +2,6 @@ package org.yaoha;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -11,28 +10,58 @@ import java.util.List;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
-import oauth.signpost.http.HttpRequest;
-
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
 
 import android.util.Log;
 
 public class ApiConnector {
-    HttpClient client;
+    DefaultHttpClient client;
     private static final String apiUrl = "api.openstreetmap.org";
-    private static final String devApiIp = "134.169.35.227";
+    private static final String devApiIp = "192.168.20.133";
     private static final int devApiPort = 3000;
     private static final String xapiUrl = "www.overpass-api.de";
+    private static String username = "foobar";
+    private static String password = "baz-word";
     
     public ApiConnector() {
+        HttpRequestInterceptor preemptiveAuth = new HttpRequestInterceptor() {
+            public void process(final org.apache.http.HttpRequest request, final HttpContext context) throws HttpException, IOException {
+                AuthState authState = (AuthState) context.getAttribute(ClientContext.TARGET_AUTH_STATE);
+                CredentialsProvider credsProvider = (CredentialsProvider) context.getAttribute(
+                        ClientContext.CREDS_PROVIDER);
+                HttpHost targetHost = (HttpHost) context.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
+                
+                if (authState.getAuthScheme() == null) {
+                    AuthScope authScope = new AuthScope(targetHost.getHostName(), targetHost.getPort());
+                    Credentials creds = credsProvider.getCredentials(authScope);
+                    if (creds != null) {
+                        authState.setAuthScheme(new BasicScheme());
+                        authState.setCredentials(creds);
+                    }
+                }
+            }
+        };
         client = new DefaultHttpClient();
+        client.addRequestInterceptor(preemptiveAuth, 0);
+        client.getCredentialsProvider().setCredentials(new AuthScope(devApiIp, devApiPort), new UsernamePasswordCredentials(username, password));
     }
     
     public InputStream getNodes(URI uri) throws ClientProtocolException, IOException {
