95fddec14e5066055b47bd572ee0797045a43264
Stefan <s.hobohm@tu-bs.de>
2012-02-04 19:38:12 +0100

    major OAuth remake, oauth now works

---
+++ src/org/yaoha/C.java
@@ -0,0 +1,25 @@
+package org.yaoha;
+
+
+public class C {
+	
+	public static final String TAG = "Yaoha";
+
+	public static final String CONSUMER_KEY 	= "LXhdgmfvvoGRmVCc0EPZajUS8458AXYZ2615f9hs";
+	public static final String CONSUMER_SECRET 	= "ZTfY5iYZ8Lszgy6DtRh0b258qciz4aYm1XnMciDi";
+
+	public static final String SCOPE 			= "http://http://www.openstreetmap.org/";
+	public static final String REQUEST_URL 		= "http://www.openstreetmap.org/oauth/request_token";
+	public static final String ACCESS_URL 		= "http://www.openstreetmap.org/oauth/access_token";  
+	public static final String AUTHORIZE_URL 	= "http://www.openstreetmap.org/oauth/authorize";
+	
+	//public static final String GET_CONTACTS_FROM_GOOGLE_REQUEST 		= "https://www.google.com/m8/feeds/contacts/default/full?alt=json";
+	
+	public static final String ENCODING 		= "UTF-8";
+	
+	public static final String	OAUTH_CALLBACK_SCHEME	= "osm-login";
+	public static final String	OAUTH_CALLBACK_HOST		= "yaoha.org";
+	public static final String	OAUTH_CALLBACK_URL		= OAUTH_CALLBACK_SCHEME + "://" + OAUTH_CALLBACK_HOST;
+	public static final String	APP_NAME                = "Yaoha";
+
+}
