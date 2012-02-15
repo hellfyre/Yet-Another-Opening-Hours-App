73c01dacddcb8ce37f917ad6a8be161e723debea
Matthias Uschok <dev@uschok.de>
2012-02-15 16:57:39 +0100

    Added new hour range dialogs

---
+++ src/org/yaoha/NodeEditAddHourRangeActivity.java
@@ -0,0 +1,14 @@
+package org.yaoha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NodeEditAddHourRangeActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+}
